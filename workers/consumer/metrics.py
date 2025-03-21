8b60e7e3e824c741a5733e4dae4d274a780450cb
Thomas Blanchot <thomas.blanchot@illuin.tech>
2025-03-21 13:03:21 +0100

    fix job

--- workers/consumer/metrics.py
+++ workers/consumer/metrics.py
@@ -5,7 +5,8 @@ from math import inf
 from typing import List, Tuple
 
 from httpx import AsyncClient
-from settings import VLLMServer, settings
+from settings import settings
+from vllm_server import VLLMServer
 
 DEFAULT_RETRY = 5
 
