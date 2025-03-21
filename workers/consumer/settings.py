234a964720433b6ef8a01cb8b438adf49c46b937
Thomas Blanchot <thomas.blanchot@illuin.tech>
2025-03-21 17:29:36 +0100

    remove unused imports

--- workers/consumer/settings.py
+++ workers/consumer/settings.py
@@ -1,6 +1,5 @@
 import json
 import logging
-from typing import List, Optional
 
 from pydantic import Field
 from pydantic_settings import BaseSettings
--- workers/sender/models.py
+++ workers/sender/models.py
@@ -1,5 +1,3 @@
-import time
-
 from httpx import AsyncClient
 from settings import Settings
 
