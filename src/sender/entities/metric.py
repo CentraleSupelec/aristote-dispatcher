a34689d082cee78d7c133bb25e3ac70a841adc63
Thomas Blanchot <thomas.blanchot@illuin.tech>
2025-12-10 12:55:51 +0100

    string instead of enum

--- src/sender/entities/metric.py
+++ src/sender/entities/metric.py
@@ -20,6 +20,4 @@ class Metric(Base):
     current_parallel_requests = Column(Integer, nullable=True)
     priority = Column(Integer, nullable=True)
     performance_score = Column(Float, nullable=True)
-    routing_mode = Column(
-        Enum("any", "private-first", "private-only", native_enum=False)
-    )
+    routing_mode = Column(String(length=255))
