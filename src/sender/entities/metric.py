f80e5aa94d67d6c7e00c51ca3312b96e01c569a0
Thomas Blanchot <thomas.blanchot@illuin.tech>
2025-12-12 14:54:13 +0100

    add organization fields to metrics table

--- src/sender/entities/metric.py
+++ src/sender/entities/metric.py
@@ -7,11 +7,13 @@ class Metric(Base):
     __tablename__ = "metrics"
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_name = Column(String(length=255), nullable=False)
+    user_organization = Column(String(length=255))
     request_date = Column(DateTime)
     sent_to_llm_date = Column(DateTime)
     response_date = Column(DateTime)
     model = Column(String(length=255))
     server = Column(String(length=255))
+    server_organization = Column(String(length=255))
     prompt_tokens = Column(Integer)
     completion_tokens = Column(Integer)
     strategy = Column(String(length=255))
